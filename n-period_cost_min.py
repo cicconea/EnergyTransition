01f580fc0f67a438b2da9e707765c9891e82bd85
aciccone <adriana.ciccone@gmail.com>
2015-01-28 21:12:56 -0600

    fixed inequality, still two commented issues

--- n-period_cost_min.py
+++ n-period_cost_min.py
@@ -6,11 +6,11 @@ import matplotlib.pyplot as plt
 Kl = 0.0
 Kh = 1000.0
 Ch_t = 1.0
-Cl_t = 3.0
-CER_t = 2.0
-alpha = 0.1
+Cl_t = 1.0
+CER_t = 1.0
+alpha = 0.001
 el = 0.0
-eh = 5000.0
+eh = 100.0 #some sort of colinearity happening in emissions constraint
 r = 0.05
 
 # TODO
@@ -38,8 +38,6 @@ def costSolver(n):
 		investCost.append(icost_t)
 		fixedCost.append(fcost_t)
 
-	print investCost
-	print fixedCost
 
 	icost = dict(zip(values, investCost))
 	fcost = dict(zip(values, fixedCost))
@@ -51,16 +49,19 @@ def costSolver(n):
 	# emissions constraint
 	# writing on several lines because otherwise impossible to read:
 	emis = eh*Kh*(alpha*float(n) - 0.5*n + 0.5) + el*Kl*float(n)*(alpha - 1) - 0.5*el*Kh*(float(n)+1)
-	cost_model += sum([variables[i] for i in values])*float(n)*(el - eh) >= emis
+	cost_model += sum([variables[i] for i in values])*float(n)*(el - eh) <= emis
 	
-	print sum([variables[i] for i in values])*float(n)*(el - eh)
-	print emis
 
 	# capital constraints 
 	for t in range(1, n+1):
 		cost_model += sum([variables[i] for i in values[:t]]) >= -Kl - Kh*(float(t)/float(n))
 		cost_model += sum([variables[i] for i in values[:t]]) <= Kh*(1 - float(t)/float(n))
 
+	# fixed capital constraints - weird that they are not automatically happening 
+	# investigate further
+	for i in values:
+		cost_model += variables[i] <= Kl + Kh
+		cost_model += variables[i] >= -Kl - Kh
 
 	cost_model.solve()
 
