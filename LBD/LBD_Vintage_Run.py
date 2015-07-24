7aaee59c5712cc816247d145ada68d1bc39e07b6
aciccone <adriana.ciccone@gmail.com>
2015-07-24 12:12:48 -0500

    Standardized file names

---
+++ LBD/LBD_Vintage_Run.py
@@ -0,0 +1,45 @@
+from LBDhelpers import *
+from LBDpyomoVintageMin import vintageModel
+import time
+
+if __name__ == "__main__":
+	'''
+	This module should be called from the command line to set up all parameters,
+	create the model, solve and plot the results. 
+	'''
+	
+	start = time.time()
+
+	# generate the parameter data from the LBDhelpers module.
+	params = genData()
+	print
+	print "\t Imported parameters successfully in ", (time.time() - start)
+
+	# Create the model
+	model = vintageModel(params)
+	print "\t Constructed model in ", (time.time() - start)
+
+	# Solve the model
+	instance, solverStatus, terminationCondition = NLmodelSolve(model)
+
+	# Get solution and constraint values
+	constraintDict = getConstraints(instance)
+	varDict = getVars(instance)
+
+
+	if str(solverStatus) != "warning":
+		print "\t Generating graphs in ", (time.time() - start)
+		genVintPlot(params, constraintDict, varDict)
+
+	print "\t Checking Constraints in ", (time.time() - start)
+	checkConstraintFeasibility(params, varDict, constraintDict)
+
+	print
+	print
+	print "\t Done in ", (time.time() - start)
+
+
+
+
+
+
